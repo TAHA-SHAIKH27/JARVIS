bcbd79b626cac024365870c0d8fcfb25afc9772c
alfiyashaikh2005 <alfiyashaikh16728@gmail.com>
2026-08-26 15:34:06 +0530

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+  allowedHosts: [".vercel.run"],
     port: 3000,
     proxy: {
       '/api': {
