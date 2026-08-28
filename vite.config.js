048f4d33ab658822828feea22e113e8943e42c10
TAHA-SHAIKH27 <tshaikh271109@gmail.com>
2026-08-28 08:31:43 +0530

    Change allowedHosts configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,7 @@ export default defineConfig({
 
   server: {
     port: 3000,
-<<<<<<< Updated upstream
-    allowedHosts: [".vercel.run"],
-=======
     allowedHosts: true,
-
->>>>>>> Stashed changes
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
